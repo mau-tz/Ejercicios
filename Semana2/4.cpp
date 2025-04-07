8b842eaedb37de7b31b8a8eb5948406663fee8c7
Fabrizio Santi <117610911+Santi2007939@users.noreply.github.com>
2025-04-07 13:12:23 -0500

    Create 4.cpp

---
+++ Semana2/4.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+#include <iostream>
+using namespace System;
+using namespace std;
+
+// Crear un lambda para determinar si la suma de 3 numeros es multiplo de 3
+
+int main()
+{
+    int num1, num2, num3;
+    cout << "Ingrese el primer numero: "; cin >> num1;
+    cout << "Ingrese el segundo numero: "; cin >> num3;
+    cout << "Ingrese el tercer numero: "; cin >> num2;
+
+    auto multiplo3 = [] (int a, int b, int c) {
+        return (a + b + c) % 3 == 0;
+       };
+
+    cout << (num1 + num2 + num3) << " ";
+    if (multiplo3(num1, num2, num3)) cout << "es multiplo de 3";
+    else cout << "no es multiplo de 3";
+
+    return 0;
+}
