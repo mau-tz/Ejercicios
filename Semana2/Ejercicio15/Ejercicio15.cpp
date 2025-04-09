32d78d282d256c3687c619465247fd0b9951422c
Fabrizio Santi <117610911+Santi2007939@users.noreply.github.com>
2025-04-09 08:05:26 -0500

    Add files via upload

---
+++ Semana2/Ejercicio15/Ejercicio15.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+#include <iostream>
+using namespace System;
+using namespace std;
+
+// Desarrollar un algoritmo con recursividad, que permita mostrar lo números Fibonacci de
+// acuerdo a un número ingresado por teclado Ejemplo, 6 = 1 1 2 3 5 8 13 21
+
+int numeros[10001];
+
+int fibonacci(int num) {
+    if (num <= 0) return 0;
+    if (numeros[num] != 0) return numeros[num];
+
+    numeros[num] = fibonacci(num - 1) + fibonacci(num - 2);
+    cout << " " << numeros[num];
+    return numeros[num];
+}
+
+int main()
+{
+    int num;
+    cout << "Ingrese un numero: ";
+    cin >> num;
+
+    numeros[1] = numeros[2] = 1;
+    cout << 1 << " " << 1;
+
+    int prueba = fibonacci(num + 2);
+}
