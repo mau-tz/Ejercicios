3d14692da69db3c955ba254c4d99d0291aedb8dd
Mauricio Teran Zavala <mteranzavala@gmail.com>
2025-04-09 08:38:48 -0500

    Add files via upload
    Ejercicios 1,2,4,11

---
+++ Semana2/Ejercicio1/Ejercicio1/Ejercicio1.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include <iostream>
+using namespace System;
+using namespace std;
+
+int main() {
+	double num1, num2, num3;
+	cout << "Ingresa el 1er numero: ";
+	cin >> num1;
+	cout << "Ingresa el 2edo numero: ";
+	cin >> num2;
+	cout << "Ingresa el 3ro numero: ";
+	cin >> num3;
+	auto raizcuadrada = [](double x) { return sqrt(x);};
+	auto sumatoria = [](auto x, auto y, auto z) {return x + y + z;};
+	cout << raizcuadrada(num1) << endl;
+	cout << raizcuadrada(num2) << endl;
+	cout << raizcuadrada(num3) << endl;
+
+	cout << "La sumatoria de los tres numeros: " << sumatoria(raizcuadrada(num1), raizcuadrada(num2), raizcuadrada(num3));
+}
